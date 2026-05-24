ec24f58c62f65d7574361269a24b251bed895cd8
techBasics1_Valerie_Melcher <Valerie.Melcher@stud.leuphana.de>
2026-05-24 18:14:20 +0200

    This is my Week 6 Tech Basics assignment. I wanted this game to play out in Waikiki as it is inspired by a vacation to Hawaii I once went on, because it was really cool there. It was fun to code the game and to figure out the storyline and the commands because of that. I had to do some troubleshooting with the functions but I think all of those are resolved now. If you still find any problems, please let me know hahaha. Anyway, I hope you have fun playing this game and that it feels like a small vacation. :)

---
+++ assignments/week6/game_week6.py
@@ -0,0 +1,275 @@
+# ==================================
+# TEXT-BASED ADVENTURE GAME WEEK 6
+# The goal is to find the golden shell
+# ==================================
+
+# ----------- Inventory ------------
+inventory = []
+max_items = 5
+
+# ------------- Player -------------
+player_health = 100
+current_room = "beach"
+
+# ------------- Rooms --------------
+rooms = {
+    "beach": {
+        "description": "Warm sand, sunny skies, the perfect place to spend a day in Waikiki",
+        "items": [
+            {"name": "shovel", "type": "tool"},
+            {"name": "sunscreen", "type": "protection"},
+            {"name": "hot sand", "type": "danger"}
+        ],
+        "surrounding places": ["surf shop", "ocean", "beach bar"]
+    },
+
+    "surf shop": {
+        "description": "A colorful store full of island keepsakes",
+        "items": [
+            {"name": "box", "type": "treasure"},
+            {"name": "fan", "type": "protection", "uses": 1}
+        ],
+        "surrounding places": ["beach"]
+    },
+
+    "ocean": {
+        "description": "Bright blue waters, refreshing to swim in the sun",
+        "items": [
+            {"name": "snorkel", "type": "tool"},
+            {"name": "sea urchin", "type": "danger"}
+        ],
+        "surrounding places": ["beach"]
+    },
+
+    "beach bar": {
+        "description": "A cool place to hang out and enjoy the beachfront view",
+        "items": [
+            {"name": "refreshing drink", "type": "protection"}
+        ],
+        "surrounding places": ["beach"]
+    }
+}
+
+# -------- Functions ---------
+
+def show_help():
+    print("""
+Commands:
+inventory
+pickup <item>
+drop <item>
+use <item>
+examine <item>
+go to the <room>
+look
+help
+quit
+""")
+
+
+def show_room():
+    room = rooms[current_room]
+
+    print("\n==", current_room.upper(), "==")
+    print(room["description"])
+
+    show_room_items()
+
+    print("Surrounding places:", ", ".join(room["surrounding places"]))
+
+
+def show_room_items():
+    room = rooms[current_room]
+
+    if not room["items"]:
+        print("No items here.")
+        return
+
+    print("Items in the room:")
+    for item in room["items"]:
+        print("–", item["name"])
+
+
+def show_inventory():
+    if len(inventory) == 0:
+        print("Your inventory is empty")
+        return
+
+    print("Inventory:")
+    for item in inventory:
+        print("–", item["name"], f"({item['type']})")
+
+
+def pick_up(item_name):
+    room = rooms[current_room]
+
+    if len(inventory) >= max_items:
+        print("Your inventory is full")
+        return
+
+    for item in room["items"]:
+        if item["name"].lower() == item_name.lower():
+            inventory.append(item)
+            room["items"].remove(item)
+            print(f"You picked up the {item_name}.")
+            return
+
+    print("This item is not found here")
+
+
+def drop(item_name):
+    room = rooms[current_room]
+
+    for item in inventory:
+        if item["name"].lower() == item_name.lower():
+            inventory.remove(item)
+            room["items"].append(item)
+            print(f"You dropped the {item_name}.")
+            return
+
+    print("You don't have that item")
+
+
+def examine(item_name):
+    for item in inventory:
+        if item["name"].lower() == item_name.lower():
+            print(f"{item_name} is a {item['type']} item.")
+            return
+
+    for item in rooms[current_room]["items"]:
+        if item["name"].lower() == item_name.lower():
+            print(f"{item_name} is a {item['type']} item.")
+            return
+
+    print(" This item cannot be found here")
+
+
+def use(item_name):
+    global player_health
+
+    for item in inventory:
+        if item["name"].lower() == item_name.lower():
+
+            if item["type"] == "protection":
+                player_health += 20
+                if player_health > 100:
+                    player_health = 100
+
+                print("You used it!")
+                print("Health:", player_health)
+
+                inventory.remove(item)
+                return
+
+            elif item["type"] == "treasure":
+                print("You opened the box but it is empty")
+                inventory.remove(item)
+                return
+
+            elif item["type"] == "danger":
+                player_health -= 30
+                print("Ouch!")
+                print("Health:", player_health)
+
+                if player_health <= 0:
+                    print("Oh no, it seems you had a heatstroke, game over :(")
+                    quit()
+
+                inventory.remove(item)
+                return
+
+            elif item["name"] == "shovel" and current_room == "beach":
+                print("You dig in the sand but there is nothing")
+                return
+
+            elif item["name"] == "snorkel" and current_room == "ocean":
+                print("You dive into the beautiful blue water...")
+                print("Suddenly you see a shiny golden shell!")
+                print("YOU WIN!")
+                quit()
+
+            else:
+                print("You can't use that here")
+                return
+
+    print("You don't have that item")
+
+
+def move(room_name):
+    global current_room
+
+    if room_name in rooms[current_room]["surrounding places"]:
+        current_room = room_name
+        show_room()
+    else:
+        print("You can't go there")
+
+
+# ------------- Game start --------------
+
+print("Aloha! Welcome to Waikiki beach!")
+print("Somewhere here is a golden shell. Find it!")
+show_help()
+show_room()
+
+# ------------ Main loop --------------
+
+while True:
+
+    command = input("\n> ").lower().split()
+
+    if len(command) == 0:
+        continue
+
+    action = command[0]
+
+    if action == "inventory":
+        show_inventory()
+
+    elif action == "pickup":
+        if len(command) < 2:
+            print("Pick up what?")
+        else:
+            item_name = " ".join(command[1:])
+            pick_up(item_name)
+
+    elif action == "drop":
+        if len(command) < 2:
+            print("Drop what?")
+        else:
+            item_name = " ".join(command[1:])
+            drop(item_name)
+
+    elif action == "use":
+        if len(command) < 2:
+            print("Use what?")
+        else:
+            item_name = " ".join(command[1:])
+            use(item_name)
+
+    elif action == "examine":
+        if len(command) < 2:
+            print("Examine what?")
+        else:
+            item_name = " ".join(command[1:])
+            examine(item_name)
+
+    elif action == "go":
+        if len(command) < 4:
+            print("Go where? Try: go to the beach")
+        else:
+            room_name = " ".join(command[3:])
+            move(room_name)
+
+    elif action == "look":
+        show_room()
+
+    elif action == "help":
+        show_help()
+
+    elif action == "quit":
+        print("Thanks for playing!")
+        break
+
+    else:
+        print("Unknown command")
